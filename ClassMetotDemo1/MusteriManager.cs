b7eae8fe0829e131a693ea12cd8c83178b262b64
Mehmet ABACIOĞLU <mabacioglu@gmail.com>
2021-01-19 00:57:21 +0300

    Add project files.

---
+++ ClassMetotDemo1/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+
+            Console.WriteLine(" {1} isimli müşteri {0} Id no ile eklenmiştir.",musteri.Id,musteri.Adi);
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+
+            Console.WriteLine(" Id no {0} olan {1} isimli müşteri silinmiştir.", musteri.Id, musteri.Adi);
+        }
+
+        public void MusteriListele(Musteri[] musteriler)
+        {
+
+            Console.WriteLine("Bütün müşteriler aşağıda listelenmiştir.");
+            foreach (Musteri item in musteriler)
+            {
+                Console.WriteLine("       Id: {0}   Adı: {1}   Soyadı:  {2}",item.Id,item.Adi,item.Soyadi);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Mehmet";
+            musteri1.Soyadi = "Abacıoğlu";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Özkan";
+            musteri2.Soyadi = "Göktaş";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriListele(musteriler);
+            musteriManager.MusteriSil(musteri1);
+
+        }
+    }
+}
